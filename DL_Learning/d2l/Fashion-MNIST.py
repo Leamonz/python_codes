25112376b4688f213cfc6ee96bbd53f9704a742f
Leamonz <3262305069@qq.com>
2022-09-17 23:14:30 +0800

    files moved to another dir dat:09-17

--- DL_Learning/d2l/Fashion-MNIST.py
+++ DL_Learning/d2l/Fashion-MNIST.py
@@ -2,7 +2,6 @@ import torch
 import torchvision
 from torch.utils import data
 from torchvision import transforms
-from d2l import torch as d2l
 
 transform = transforms.Compose([
     transforms.ToTensor(),
