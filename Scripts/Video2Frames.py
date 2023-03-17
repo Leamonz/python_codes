9dacd1b35434f0c7f33fa0f7463ce8a41bf8f03e
Leamonz <3262305069@qq.com>
2023-03-17 23:17:40 +0800

    Convert video to frames

--- Scripts/Video2Frames.py
+++ Scripts/Video2Frames.py
@@ -1,7 +1,14 @@
+import PIL.Image
 import cv2
 import glob
 import os
 from datetime import datetime
+
+import numpy as np
+import torchvision.transforms
+
+VIDEO_PATH = r"D:\RawData\014\background_2\profile\normal\right_60\VID_20230305_154038.mp4"
+SAVE_PATH = r'D:\RawData\014\background_2\profile\normal\right_60'
 
 
 def video_to_frames(path):
@@ -14,14 +21,20 @@ def video_to_frames(path):
     print("fps=", int(fps), "frames=", int(frames))
 
     for i in range(int(frames)):
+        print(i + 1)
         ret, frame = videoCapture.read()
-        cv2.imwrite("frames/%d.jpg" % (i + 1), frame)
+        transform = torchvision.transforms.Compose(
+            [torchvision.transforms.RandomVerticalFlip(p=1),
+             torchvision.transforms.RandomHorizontalFlip(p=1)]
+        )
+        frame = np.asarray(transform(PIL.Image.fromarray(frame)))
+        cv2.imwrite("%s/%d.jpg" % (SAVE_PATH, i + 1), frame)
     return
 
 
 if __name__ == '__main__':
     t1 = datetime.now()
-    video_to_frames("1.mp4")
+    video_to_frames(VIDEO_PATH)
     t2 = datetime.now()
     print("Time cost = ", (t2 - t1))
     print("SUCCEED !!!")
